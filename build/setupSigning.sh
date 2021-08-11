d32792e081496016e4e7cc411222b9169474f58f
shyamalagowri <shyamalagowri@192.168.1.2>
2021-08-11 22:03:00 +0530

    updated encoded key file

--- build/setupSigning.sh
+++ build/setupSigning.sh
@@ -11,7 +11,6 @@ echo "Importing signing key..."
 # Modify the command below to use the correct environment variables
 # that were added to your Travis build settings when you encrypted your signing.key file.
 openssl aes-256-cbc -K $encrypted_4b7d603e7466_key -iv $encrypted_4b7d603e7466_iv -in build/signing.key.enc -out build/signing.key -d
-
 gpg --version
 gpg --fast-import build/signing.key
 rm build/signing.key
